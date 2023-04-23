3e2e887e15ba34c19140193c14366aae7551d3e0
imanolmatos <imanolmatos007@gmail.com>
2023-04-22 20:37:39 -0400

    The two programs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,5 +9,10 @@ class Program
         Console.WriteLine("What is your last name? ");
         string last_name  = Console.ReadLine();
         Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        
+
+
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+                Console.WriteLine("Other Program: ");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade  = Console.ReadLine();
+        float numberGrade = int.Parse(grade);       
+        string letter = "";
+        if (numberGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"{letter}.");
+        if (numberGrade >=70)
+        {
+            Console.WriteLine("Congratulations, you approved the coursed! ");
+        }
+        else
+        {
+             Console.WriteLine("Sorry, you can do it! Try next time. ");
+        }
     }
 }
