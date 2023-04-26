9a9f05f24c7fcd436dbed9ca85d1b3eb98b2a4e0
imanolmatos <imanolmatos007@gmail.com>
2023-04-26 16:00:09 -0400

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,20);
+        float numberGuess = 1;
+        while (number != numberGuess)
+        {
+            // Console.WriteLine("What is the magic number? ");
+            // string magic  = Console.ReadLine();
+            // number = float.Parse(magic);    
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            numberGuess = float.Parse(guess);
+
+            if (number > numberGuess)
+            {Console.WriteLine("Higher");
+            }
+
+            else if (number < numberGuess)
+            {Console.WriteLine("Lower");
+
+            }
+            else 
+            {Console.WriteLine("You guessed it!");
+            }
+        }
+
+
+
+
+//         while magic_number != guess:
+//     if magic_number > guess:
+//         print("Higher")
+//     elif magic_number < guess:
+//         print("Lower")
+//     guess = int(input("What is your guess? "))
+// else: 
+//     print("You guessed it!")
+
+
+
     }
 }
